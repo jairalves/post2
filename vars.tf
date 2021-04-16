a40382b82bb36781c3b498b6a7b2b5bc37ac1e07
JAIR ALVES DE SOUZA JUNIOR <jalves@lmcorp.local>
2021-04-16 11:08:17 -0300

    Utilizar Template atualizado 2

--- vars.tf
+++ vars.tf
@@ -1,3 +1,9 @@
-variable "vsphere_user" {}
+variable "vsphere_user" {
+    type = string
+    default = "administrator@vsphere.local"
+}
+variable "vsphere_server" {
+    type = string
+    default = "10.10.10.1"
+}
 variable "vsphere_password" {}
-variable "vsphere_server" {}
